bce6f53986118dd38ebfc721c84b9fe04e817236
kumarshailesh9 <theshaileshkuma9@gmail.com>
2025-12-13 15:03:23 +0530

    remove footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import Services from "./components/Card";
 import Footer from "./components/Footer";
-import NavBar from "./components/Navbar";
 import HomePage from "./Home/page";
 
 export default function Home() {
   return (
     <>
     <HomePage/>
-    <Footer/>
     </>
   );
 }
